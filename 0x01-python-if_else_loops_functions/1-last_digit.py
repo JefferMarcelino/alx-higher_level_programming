0b727a9ad17d2ba86e613dadd11286d547ec1670
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-03-08 22:57:52 +0200

    fix(0x01-python-if_else_loops_functions - task 1): break a line to follow pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,7 +11,8 @@ elif number < 0:
 if lastDigit > 5:
     print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif lastDigit < 6 and lastDigit != 0:
-    print(f"Last digit of {number} is {lastDigit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {lastDigit} and is \
+less than 6 and not 0")
 elif lastDigit == 0:
     print(f"Last digit of {number} is {lastDigit} and is 0")
 else:
