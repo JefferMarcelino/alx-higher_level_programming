aba82c7ea19d1b12b2273dac7549786ab602581e
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-03-12 18:09:08 +0200

    fix(0x02-python-import_modules - task 4): fix the pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    defined_names = [name for name in dir(hidden_4) if not name.startswith("_")]
+    defined_names = dir(hidden_4)
     for name in defined_names:
-        print(name)
+        if not name.startswith("_"):
+            print(name)
