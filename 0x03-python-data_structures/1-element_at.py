05d415ddfc2de4c05ac9086de8101739fe564996
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-03-13 18:33:59 +0200

    feat(0x03-python-data_structures - task 1): a function that retrieves an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """A function that retrieves an element on the list
+    at a given index
+
+    Args:
+        my_list: the list
+        idx: the given index
+
+    Return:
+        The element at the given index, otherwise None
+    """
+    if len(my_list) - 1 > idx >= 0:
+        return (my_list[idx])
+    return (None)
