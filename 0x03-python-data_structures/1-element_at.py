d1aac422068b7f691d3443dc26ee4f4617240a3e
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-03-13 18:42:41 +0200

    fix(0x03-python-data_structures - task 1): statement condition to return none

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -10,6 +10,6 @@ def element_at(my_list, idx):
     Return:
         The element at the given index, otherwise None
     """
-    if len(my_list) - 1 > idx >= 0:
-        return (my_list[idx])
-    return (None)
+    if len(my_list) - 1 < idx or idx < 0:
+        return (None)
+    return (my_list[idx])
