abd6a20f816259051865a063ca32a216886e545b
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-03-13 18:17:41 +0200

    feat(0x03-python-data_structures - task 0): a function that prints all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,11 @@
+def print_list_integer(my_list=[]):
+    """A function that prints a list of integers
+
+    Args:
+        my_list: the list of integers
+
+    Return:
+        nothing
+    """
+    for integer in my_list:
+        print("{:d}".format(integer))
